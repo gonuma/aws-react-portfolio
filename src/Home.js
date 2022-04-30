7a8604b32861fa1181f4975e79a31e9ed77eaf01
gonuma <81454703+gonuma@users.noreply.github.com>
2022-04-30 14:07:03 +0900

    Init commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <p>This is home</p>
+    </div>
+  );
+};
+
+export default Home;
