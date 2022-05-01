1843b8386081688638d4507a7e8906da6527ec47
gonuma <81454703+gonuma@users.noreply.github.com>
2022-05-01 13:47:39 +0900

    Basic Routing Added

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,36 @@
 import React from "react";
+import "antd/dist/antd.css";
+import { Row, Col, Button, Divider, Icon } from "antd";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h1>This is the header</h1>
-    </div>
+    <>
+      {/* <Divider orientation="center"></Divider> */}
+      <Row justify="space-evenly" style={{ marginTop: 20 }}>
+        <Col>
+          <Link to="/">
+            <Button>About</Button>
+          </Link>
+        </Col>
+        <Col>
+          <Link to="/tech">
+            <Button>Tech</Button>
+          </Link>
+        </Col>
+        <Col>
+          <Link to="/music">
+            <Button>Music</Button>
+          </Link>
+        </Col>
+        <Col>
+          <Link to="/cycling">
+            <Button>Cycling</Button>
+          </Link>
+        </Col>
+      </Row>
+      <Divider orientation="center"></Divider>
+    </>
   );
 };
 
---
+++ src/Music.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Music = () => {
+  return (
+    <div>
+      <p>This is music</p>
+    </div>
+  );
+};
+
+export default Music;
