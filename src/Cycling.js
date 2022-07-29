a4fc4cfd1d4aede2f3f9956211b27693b83cb2d5
Greg <greg@localhost.localdomain>
2022-07-29 10:45:03 +0900

    Swapped 2 images for placeholders

--- src/Cycling.js
+++ src/Cycling.js
@@ -18,7 +18,7 @@ export default function Cycling() {
           width: "inherit",
           objectFit: "cover",
         }}
-        src="https://s3.ap-northeast-1.amazonaws.com/www.gonuma.com/bike.jpg"
+        src="https://asset.scott-sports.com/foi/foil-launch-pro_1600x750_landing-main-banner_2022_BIKE_SCOTT-Sports.jpg?signature=1c60fb30d8cda7d121caff882affcfe6a3448f0de0bb84e5dc52daa027d0be20"
       />
     </div>
   );
